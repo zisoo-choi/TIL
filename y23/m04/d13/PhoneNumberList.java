a6962b5335a9a46bdcf96aba20667071ea7bd3f6
zisoo <zisoo_3@naver.com>
2023-04-13 23:45:31 +0900

    readme 작성바람

--- y23/m04/d13/PhoneNumberList.java
+++ y23/m04/d13/PhoneNumberList.java
@@ -1,4 +1,30 @@
 package y23.m04.d13;
 
+import java.util.*;
+
 public class PhoneNumberList {
+    public static void main(String[] args) {
+        // 문제에서 주어지는 값
+        String[] phone_book = {"12","567","88","123"};
+        boolean answer = true;
+
+        HashSet<String> set = new HashSet<>();
+
+        for(String phone : phone_book){
+            set.add(phone);
+        }
+
+        for(String phone : phone_book){
+            for(int i = 0; i < phone.length(); i++){
+                if(set.contains(phone.substring(0, i))) {
+                    answer = false;
+                    System.out.println(answer);
+                    // 문제에서는 여기서 리턴된다.
+                }
+            }
+        }
+        answer = true;
+        System.out.println(answer);
+        // 문제에서는 여기서 리턴된다.
+    }
 }
---
+++ y23/m04/d13/readme.md
@@ -0,0 +1,7 @@
+# 2022.04.13
+
+프로그래머스 > 해시 > [전화번호 목록](https://school.programmers.co.kr/learn/courses/30/lessons/42577)
+
+리뷰는 정신차리고 내일 하겠음.
+
+
